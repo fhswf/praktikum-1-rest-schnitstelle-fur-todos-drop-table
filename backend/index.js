70458ac12e04eaef90d0b5a9a3111cc6d57cf921
Patrick <75327256+Komo92@users.noreply.github.com>
2023-02-26 11:47:57 +0000

    Aufgabe 1

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 /** Zentrales Objekt für unsere Express-Applikation */
 const app = express();
-
+const port = 3000
 /**
  * Liste aller ToDos. 
  * Wird später durch Datenbank ersetzt!
@@ -22,5 +22,60 @@ let TODOS = [
     },
 ];
 
-// Your code here
-console.log('Hier ist alles anders'bhhhhhhhhhhhhb)
+
+/*Operation 	Methode 	URL 	Beschreibung
+Create 	POST 	/todos 	Anlegen eines neuen ToDos. Die ID wird dabei vom Backend vergeben.
+Read 	GET 	/todos/:id 	Lesen des ToDos mit ID id. Die Syntax :id wird von Express.js verwendet,
+um Path Parameter zu spezifizieren.
+Read 	GET 	/todos 	Lesen der Liste aller ToDos.
+Update 	PUT 	/todos/:id 	Update des ToDos mit ID id.
+Delete 	DELETE 	/todos/:id 	Löschen des ToDos mit ID id.*/
+
+//Description of the API 
+app.get('/', (req, res) => {
+    res.send('Todo App API\n' + 'reate Todo: POST /todos\n' + 'Read Todo: GET /todos/:id\n' + 'Read all Todos: GET /todos\n' + 'Update Todo: PUT /todos/:id\n' + 'Delete Todo: DELETE /todos/:id\n');
+})
+
+//Get all todos
+app.get('/todos', (req, res) => {
+    res.send(TODOS);
+  })
+
+//Get a single todo
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const todo = TODOS.find(t => t.id == id);
+    res.send(todo);
+  }
+)
+
+//Create a new todo
+app.post('/todos', (req, res) => {
+    const todo = req.body;
+    const maxId = TODOS.reduce((max, t) => t.id > max ? t.id : max, 0);
+    todo.id = maxId + 1;
+    TODOS.push(todo);
+    res.send(todo);
+  }
+)
+
+//Update a todo
+app.put('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const todo = TODOS.find(t => t.id == id);
+    Object.assign(todo, req.body);
+    res.send(todo);
+  }
+)
+
+//Delete a todo
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    TODOS = TODOS.filter(t => t.id != id);
+    res.send();
+  }
+)
+
+app.listen(port, () => {
+    console.log(`Todo App listening on port ${port}`)
+  })
